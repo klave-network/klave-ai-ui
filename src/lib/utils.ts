9c69e2e930742500b1d81d9dd1254579dc951030
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-05-20 16:24:12 +0200

    feat: Create helper functions for a promise-delay and generation of ids

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,17 @@ import type { ClassValue } from 'clsx';
 export function cn(...inputs: Array<ClassValue>) {
     return twMerge(clsx(inputs));
 }
+
+export function delay() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Promise resolved after 20 seconds');
+        }, 20000); // 20,000 milliseconds = 20 seconds
+    });
+}
+
+export function generateSimpleId(length: number = 8): string {
+    return Math.random()
+        .toString(36)
+        .substring(2, 2 + length);
+}
