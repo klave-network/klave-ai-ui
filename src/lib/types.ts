596ca70ab46d60867557368dcc52804a5b33a4c6
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-05-20 16:24:27 +0200

    chore: Add types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,59 @@ export type KeyPair = EncryptedKeyPair & {
     id: string;
     name: string;
 };
+
+export type Model = {
+    access: number;
+    description: string;
+    encryption_type: number;
+    file_size: number;
+    hash: string[];
+    hash_type: number;
+    inactivity_timeout: number;
+    is_loaded: boolean;
+    local_path: string;
+    max_concurrent_queries: number;
+    max_concurrent_queries_per_user: number;
+    max_threads: number;
+    model_format: number;
+    name: string;
+    status: number;
+    system_prompt: number[];
+    tensor_type: number;
+    tokenizer_name: string;
+    url: string;
+};
+
+export type Tokenizer = {
+    access: number;
+    description: string;
+    encryption_type: number;
+    file_size: number;
+    hash: string[];
+    hash_type: number;
+    inactivity_timeout: number;
+    is_loaded: boolean;
+    local_path: string;
+    model_format: number;
+    name: string;
+    status: number;
+    tensor_type: number;
+    url: string;
+};
+
+export type ContextInput = {
+    model_name: string;
+    context_name: string;
+    system_prompt: string;
+    temperature: number;
+    topp: number;
+    steps: number;
+    sliding_window: boolean;
+};
+
+export type PromptInput = {
+    context_name: string;
+    user_prompt: string;
+};
+
+export type Input = Pick<PromptInput, 'context_name'>;
