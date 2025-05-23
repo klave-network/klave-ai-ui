9f589852ed0d5097d740bf66cb260516d9e26478
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-05-23 12:40:23 +0200

    feat: Display models in model-selector compoennt

--- src/components/model-selector.tsx
+++ src/components/model-selector.tsx
@@ -7,21 +7,32 @@ import {
     SelectTrigger,
     SelectValue
 } from '@/components/ui/select';
+import { useUserModels } from '@/store';
+import { CUR_MODEL_KEY, CUR_USER_KEY } from '@/lib/constants';
 
 export const ModelSelector = () => {
+    const currentUser = localStorage.getItem(CUR_USER_KEY) ?? '';
+    const models = useUserModels(currentUser);
+    const currentModel = localStorage.getItem(CUR_MODEL_KEY) ?? models[0].name;
+
     return (
-        <Select>
-            <SelectTrigger className="w-[180px] border-none shadow-none">
+        <Select
+            defaultValue={currentModel}
+            onValueChange={(value) =>
+                localStorage.setItem(CUR_MODEL_KEY, value)
+            }
+        >
+            <SelectTrigger className="w-[180px] rounded-full">
                 <SelectValue placeholder="Language model" />
             </SelectTrigger>
             <SelectContent>
                 <SelectGroup>
                     <SelectLabel>Available language models</SelectLabel>
-                    <SelectItem value="abcd">ABCD</SelectItem>
-                    <SelectItem value="efgh">EFGH</SelectItem>
-                    <SelectItem value="ijkl">IJKL</SelectItem>
-                    <SelectItem value="mnop">MNOP</SelectItem>
-                    <SelectItem value="qrst">QRST</SelectItem>
+                    {models.map((model) => (
+                        <SelectItem key={model.name} value={model.name}>
+                            {model.name}
+                        </SelectItem>
+                    ))}
                 </SelectGroup>
             </SelectContent>
         </Select>
