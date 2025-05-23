b4d873f228f7242e12bf9b82574a3b27f13b42b4
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-05-23 11:57:14 +0200

    feat: Update metadata, change favicon, create Logo component

--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -1,20 +1,11 @@
-import { Circle } from 'lucide-react';
 import { SidebarMenu, SidebarMenuItem } from '@/components/ui/sidebar';
-import { Link } from '@tanstack/react-router';
+import { Logo } from '@/components/logo';
 
 export function NavHeader() {
     return (
         <SidebarMenu>
             <SidebarMenuItem>
-                <Link
-                    to="/"
-                    className="flex items-center gap-2 self-center font-medium p-2"
-                >
-                    <div className="flex size-8 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                        <Circle className="size-4" />
-                    </div>
-                    Sanctum
-                </Link>
+                <Logo className="p-2" />
             </SidebarMenuItem>
         </SidebarMenu>
     );
--- src/routes/login/route.tsx
+++ src/routes/login/route.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router';
-import { Circle } from 'lucide-react';
+import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { Logo } from '@/components/logo';
 
 export const Route = createFileRoute('/login')({
     component: RouteComponent
@@ -9,15 +9,7 @@ function RouteComponent() {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
-                <Link
-                    to="/"
-                    className="flex items-center gap-2 self-center font-medium"
-                >
-                    <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                        <Circle className="size-4" />
-                    </div>
-                    Sanctum
-                </Link>
+                <Logo />
                 <Outlet />
             </div>
         </div>
