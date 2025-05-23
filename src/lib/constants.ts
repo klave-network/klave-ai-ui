33504b3bfd2c0d6f0aa9a5b815394a451a49a0d5
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-05-23 12:40:33 +0200

    chore: Update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,5 +2,7 @@
 export const ENC_PWD = 'demoKlave';
 export const LOC_KEY = 'sanctum:EncKey';
 export const STORE_KEY = 'sanctum:Store';
+export const CUR_USER_KEY = 'sanctum:CurrentUser';
+export const CUR_MODEL_KEY = 'sanctum:CurrentModel';
 export const KLAVE_CONNECTION_KEYPAIR_NAME = 'klave';
 export const KLAVE_CONNECTION_KEYPAIR_PWD = 'tutu@';
