2f4fa151865af5abb208a260b9b2230eb9f4012e
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-05-23 16:58:51 +0200

    feat: Add a mode selector

---
+++ src/components/mode-selector.tsx
@@ -0,0 +1,32 @@
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue
+} from '@/components/ui/select';
+import { CUR_MODE_KEY } from '@/lib/constants';
+
+export const ModeSelector = () => {
+    const currentModel = localStorage.getItem(CUR_MODE_KEY) ?? 'generate';
+
+    return (
+        <Select
+            defaultValue={currentModel}
+            onValueChange={(value) => localStorage.setItem(CUR_MODE_KEY, value)}
+        >
+            <SelectTrigger className="w-[180px] rounded-full">
+                <SelectValue placeholder="Mode" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Available modes</SelectLabel>
+                    <SelectItem value="generate">Generate</SelectItem>
+                    <SelectItem value="chat">Chat</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    );
+};
