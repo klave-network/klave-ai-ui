e41514c3751d5f427e4c98c0b2690039118bd50e
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-07-04 12:53:56 +0200

    chore: Hide /lense endpoint

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { LifeBuoy, Settings, MessageCirclePlus, Video } from 'lucide-react';
+import { LifeBuoy, Settings, MessageCirclePlus } from 'lucide-react';
 import { NavHeader } from '@/components/nav/header';
 import { NavAdmin } from '@/components/nav/admin';
 import { NavChats } from '@/components/nav/chats';
@@ -25,13 +25,13 @@ const routes = {
             url: '/chat',
             icon: MessageCirclePlus,
             className: 'bg-gradient-to-br from-kcy to-kbl/90 text-white'
-        },
-        {
-            title: 'Lense',
-            url: '/chat/video',
-            icon: Video,
-            className: 'bg-gradient-to-br from-kbl to-kor/90 text-white'
         }
+        // {
+        //     title: 'Lense',
+        //     url: '/chat/video',
+        //     icon: Video,
+        //     className: 'bg-gradient-to-br from-kbl to-kor/90 text-white'
+        // }
     ],
     navSecondary: [
         {
