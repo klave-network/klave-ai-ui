69b4dbd9430437735fea037354e7ff509e452020
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-07-11 14:35:34 +0200

    chore: Remove 'for'

--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -5,8 +5,9 @@ export function NavHeader() {
     return (
         <SidebarMenu>
             <SidebarMenuItem>
-                <div className='pl-2 flex gap-2 items-start justify-start'>
-                    <Logo type='horizontal' className="h-6" /><span className='text-2xl'>for AI</span>
+                <div className="pl-2 flex gap-2 items-start justify-start">
+                    <Logo type="horizontal" className="h-6" />
+                    <span className="text-3xl">AI</span>
                 </div>
             </SidebarMenuItem>
         </SidebarMenu>
