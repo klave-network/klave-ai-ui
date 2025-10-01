56bb248ea7ceb6d6d90eddc8edc0a918fd6c513b
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-10-01 13:38:09 +0200

    feat: Add web server

---
+++ server/app.ts
@@ -0,0 +1,168 @@
+import type { FastifyInstance } from 'fastify';
+
+import { Utils } from '@secretarium/connector';
+import { Buffer } from 'node:buffer';
+import crypto from 'node:crypto';
+import fs from 'node:fs';
+import path from 'node:path';
+import { pipeline } from 'node:stream';
+import util from 'node:util';
+import prettyBytes from 'pretty-bytes';
+import { v4 as uuid } from 'uuid';
+
+import KeyHolder from './utils/key-holder';
+
+const pump = util.promisify(pipeline);
+
+const uploadFolder = process.env.VITE_APP_UPLOAD_FOLDER ?? 'uploads';
+const fileSizeLimit = typeof process.env.VITE_MAX_FILE_SIZE === 'string' ? Number.parseInt(process.env.VITE_MAX_FILE_SIZE) : 8589934592;
+const definitions = process.env.VITE_KLAVE_DISPATCH_ENDPOINTS?.split(',') ?? [];
+const endpoints = definitions.map((def: string) => def.split('#') as [string, string]).filter((def: [string, string]) => def.length === 2);
+// const connectionPool = new Map<string, WebSocket>();
+
+/* eslint-disable-next-line */
+export interface AppOptions { }
+
+export async function app(fastify: FastifyInstance) {
+    fastify.log.info(endpoints, 'Preparing for enpoints');
+
+    fastify.post('/file', async (req, res) => {
+        const data = await req.file();
+
+        if (!data)
+            return res.status(400).send({ error: 'No file uploaded' });
+
+        const tokenField = data.fields.token;
+        const tokenPart = Array.isArray(tokenField) ? tokenField[0] : tokenField;
+        const tokenB64 = tokenPart?.type === 'field' ? tokenPart.value as string : undefined;
+
+        if (!tokenB64)
+            return res.status(400).send({ error: 'Token is missing' });
+
+        console.info('Received upload token', tokenB64);
+        const token = Utils.fromBase64(tokenB64);
+        const tokenContent = token.subarray(0, 32 + 8);
+        const tokenContentB64 = Utils.toBase64(tokenContent, false);
+        const expectedDigest = tokenContent.subarray(0, 32);
+        const signature = token.subarray(32 + 8);
+
+        if (!KeyHolder.driveKey)
+            return res.status(500).send({ error: 'The data drive key is missing' });
+        if (!KeyHolder.signKey)
+            return res.status(500).send({ error: 'The signing key is missing' });
+
+        console.info('Verifying signature for token', tokenContentB64);
+        const public_key_buf = await crypto.subtle.exportKey('spki', KeyHolder.driveKey);
+        const public_key = Utils.toBase64(new Uint8Array(public_key_buf), false);
+        console.info('Drive public key: ', public_key);
+        console.info('Signature: ', Utils.toBase64(signature, false));
+        const isValid = await crypto.subtle.verify({ name: 'ECDSA', hash: 'SHA-256' }, KeyHolder.driveKey, signature, Buffer.from(tokenContentB64));
+
+        if (!isValid)
+            return res.status(400).send({ error: 'The signature could not be verified' });
+
+        const stream = data.file;
+        const hash = crypto.createHash('sha256');
+        let readBytes = 0;
+
+        stream.pause();
+
+        /* if the client cancelled the request mid-stream it will throw an error */
+        stream.on('error', () => {
+            res.status(400).send({ error: 'The client aborted the upload' });
+        });
+
+        stream.on('data', (chunk) => {
+            readBytes += chunk.length;
+            if (readBytes > fileSizeLimit) {
+                stream.destroy();
+                console.log(`The file should not be larger than ${prettyBytes(fileSizeLimit)}`);
+                return;
+            }
+            hash.update(chunk);
+        });
+
+        const fileName = Utils.toBase64(expectedDigest, true);
+        const destinationBase = path.join(__dirname, uploadFolder, `${fileName[0]}`, `${fileName[1]}`, `${fileName[2]}`, `${fileName[3]}`, `${fileName[4]}`);
+        fs.mkdirSync(destinationBase, {
+            recursive: true
+        });
+
+        const temporaryFileName = uuid({ random: new Uint8Array(expectedDigest) });
+        const temporaryDestination = path.join(destinationBase, temporaryFileName);
+
+        const finishProcessing = async () => {
+            const digest = hash.digest();
+            const digestB64 = Utils.toBase64(digest, true);
+
+            if (fileName !== digestB64) {
+                fs.rmSync(temporaryDestination);
+                return res.status(400).send({ error: 'The file digest did not match the expected value' });
+            }
+            else {
+                const destination = path.join(destinationBase, fileName);
+                fs.renameSync(temporaryDestination, destination);
+            }
+
+            if (!KeyHolder.signKey) {
+                return res.status(500).send({ error: 'The signing key is missing' });
+            }
+
+            const proofSign = await crypto.subtle.sign({ name: 'ECDSA', hash: 'SHA-256' }, KeyHolder.signKey, Buffer.from(tokenContentB64));
+            const resultToken = new Uint8Array(40 + 64);
+            resultToken.set(tokenContent, 0);
+            resultToken.set(new Uint8Array(proofSign), 40);
+
+            const resultTokenB64 = Utils.toBase64(resultToken, false);
+            res.status(200).send({ ok: true, uploadToken: resultTokenB64 });
+        };
+
+        stream.on('close', () => {
+            finishProcessing().catch((error) => {
+                console.error('Error while processing the file', error);
+                res.status(500).send({ error: 'An error occurred while processing the file' });
+            });
+        });
+
+        await pump(data.file, fs.createWriteStream(temporaryDestination));
+    });
+
+    fastify.get('/file', async (req, res) => {
+        res.header('content-type', 'application/json');
+        const data = req.headers.authorization?.split(';').find((part: string) => part.startsWith('klavedr='));
+
+        if (!data)
+            return res.status(400).send({ error: 'No query provided' });
+
+        const tokenB64 = data.split('=')[1];
+
+        if (!tokenB64)
+            return res.status(400).send({ error: 'Token is missing' });
+
+        const token = Utils.fromBase64(tokenB64);
+        const tokenContent = token.subarray(0, 32 + 8);
+        const digest = tokenContent.subarray(0, 32);
+
+        const fileName = Utils.toBase64(digest, true);
+        const locationBase = path.join(__dirname, uploadFolder, `${fileName[0]}`, `${fileName[1]}`, `${fileName[2]}`, `${fileName[3]}`, `${fileName[4]}`);
+        const location = path.join(locationBase, fileName);
+
+        const stream = fs.createReadStream(location);
+
+        res.header('content-type', 'application/octet-stream');
+        res.header('content-disposition', `attachment; filename="${fileName}"`);
+
+        return res.send(stream);
+    });
+
+    fastify.get('/version', async (__unusedReq, res) => {
+        await res.status(200).send({
+            version: {
+                name: process.env.VITE_TASK_TARGET_PROJECT,
+                commit: process.env.GIT_REPO_COMMIT?.substring(0, 8),
+                branch: process.env.GIT_REPO_BRANCH,
+                version: process.env.GIT_REPO_VERSION
+            }
+        });
+    });
+}
---
+++ server/utils/key-holder.ts
@@ -0,0 +1,197 @@
+import type { ClearKeyPair } from '@secretarium/connector';
+
+import { Key, SCP, Utils } from '@secretarium/connector';
+import { subtle } from 'node:crypto';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+// ES module equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log('ENV', process.env);
+const keyFolder = process.env.VITE_APP_KEY_FOLDER ?? 'keys';
+const idKeyName = process.env.IKEY ?? 'i.key.pem';
+const signKeyName = process.env.SKEY ?? 's.key.pem';
+const driveKeyName = process.env.RKEY ?? 'r.key.pem';
+const deploymentId = process.env.DID ?? process.env.VITE_APP_KLAVE_FQDN_DRIVE ?? 'main-dw.cd040f72.klave-drive.Klave-AI-Drive.klave.network';
+const secretariumEndpoint = process.env.VITE_APP_SECRETARIUM_GATEWAYS_4?.split('#')?.[2] ?? 'wss://klave-prod.secretarium.org';
+
+const klave = new SCP({
+    logger: {
+        info: (...args: unknown[]) => console.warn(...args),
+        warn: (...args: unknown[]) => console.warn(...args),
+        error: (...args: unknown[]) => console.error(...args),
+        debug: (...args: unknown[]) => console.warn(...args)
+    }
+});
+
+export class KeyHolder {
+    static idKey?: Key;
+    static signKey?: CryptoKey;
+    static driveKey?: CryptoKey;
+    static async loadServerIdentity() {
+        const keysPath = path.resolve(path.join(__dirname, keyFolder));
+        console.info('Keys path:', keysPath);
+        const idKeyPath = path.resolve(keysPath, idKeyName);
+        if (!fs.existsSync(idKeyPath)) {
+            fs.mkdirSync(keysPath, { recursive: true });
+            console.info('Generating Klave identity...');
+            KeyHolder.idKey = await Key.createKey();
+            await fs.writeFileSync(idKeyPath, JSON.stringify(await KeyHolder.idKey.exportKey()));
+        }
+        else {
+            console.info('Loading Klave identity...');
+            const idKeyContent = fs.readFileSync(idKeyPath);
+            console.info('Id key content:', idKeyContent.toString());
+            const idKey = JSON.parse(idKeyContent.toString()) as ClearKeyPair;
+            KeyHolder.idKey = await Key.importKey(idKey);
+        }
+
+        klave.onError((e) => {
+            console.error(e);
+        });
+
+        await klave.connect(secretariumEndpoint, KeyHolder.idKey);
+
+        await new Promise<void>((gresolve, greject) => {
+            let passCount = 0;
+            const blockUntilAdmin = async () => {
+                if (passCount === 1) {
+                    console.info('Waiting to be granted rights...');
+                }
+                passCount++;
+                const tx = klave.newTx(deploymentId, 'getUserContent', undefined, {});
+                const userContent: any = await new Promise((resolve, reject) => {
+                    tx.onResult((result) => {
+                        if (result.result)
+                            return resolve(result.result);
+                        if (result.message === 'User not found') {
+                            const txcu = klave.newTx(deploymentId, 'createUserRequest', undefined, {
+                                driveId: 'super',
+                                role: 'admin'
+                            });
+                            txcu.onResult(() => {
+                                return setTimeout(blockUntilAdmin, 5000);
+                            });
+                            txcu.onError((error) => {
+                                console.error('Could not create user', error);
+                                reject(new Error('Could not create user'));
+                            });
+                            txcu.send();
+                        }
+                        else {
+                            return setTimeout(blockUntilAdmin, 5000);
+                        }
+                    });
+                    tx.onError((error) => {
+                        console.error('User info could not be fetched', error);
+                        reject(new Error('User info could not be fetched'));
+                    });
+                    tx.send();
+                });
+                if (Array.isArray(userContent.roles)) {
+                    const drive = userContent.roles[0];
+                    if (drive) {
+                        if (drive.role === 'admin') {
+                            return gresolve();
+                        }
+                        else {
+                            return setTimeout(blockUntilAdmin, 5000);
+                        }
+                    }
+                    else {
+                        return setTimeout(blockUntilAdmin, 5000);
+                    }
+                }
+            };
+            blockUntilAdmin().catch(greject);
+        });
+
+        const signKeyPath = path.resolve(keysPath, signKeyName);
+        if (!fs.existsSync(signKeyPath)) {
+            // throw new Error('The signing key must be a file');
+            console.info('Obtaining the signing key...');
+            const txr = klave.newTx(deploymentId, 'resetIdentities', undefined, { resetKlaveServer: true, resetStorageServer: true });
+            await new Promise((resolve, reject) => {
+                txr.onResult((result) => {
+                    if (result.success)
+                        return resolve(result.message);
+                    reject(new Error('Identity reset failed'));
+                });
+                txr.onError((error) => {
+                    console.error('Indentity reset failed:', error);
+                    reject(new Error('Identity reset failed'));
+                });
+                txr.send();
+            });
+            const txe = klave.newTx(deploymentId, 'exportStorageServerPrivateKey', undefined, { format: 'pkcs8' });
+            const newSignKeyB64: string = await new Promise((resolve, reject) => {
+                txe.onResult((result) => {
+                    console.info('Key export result:', result);
+                    if (result.success)
+                        return resolve(result.message);
+                    reject(new Error('Key export failed'));
+                });
+                txe.onError((error) => {
+                    console.error('Key export failed:', error);
+                    reject(new Error('Key export failed'));
+                });
+                txe.send();
+            });
+            const newSignKeyContent = Utils.fromBase64(newSignKeyB64);
+            fs.writeFileSync(signKeyPath, newSignKeyContent);
+        }
+        const signKeyContent = fs.readFileSync(signKeyPath);
+        KeyHolder.signKey = await subtle.importKey(
+            'pkcs8',
+            signKeyContent,
+            {
+                namedCurve: 'P-256',
+                name: 'ECDSA'
+            },
+            true,
+            ['sign']
+        );
+
+        const driveKeyPath = path.resolve(keysPath, driveKeyName);
+        if (!fs.existsSync(driveKeyPath)) {
+            console.info('Obtaining the drive key...');
+            // throw new Error('The signing key must be a file');
+            const tx = klave.newTx(deploymentId, 'getPublicKeys', undefined, {});
+            const newDriveKeyPEM: string = await new Promise((resolve, reject) => {
+                tx.onResult((result) => {
+                    if (result.result)
+                        return resolve(result.result.klaveServerPublicKey);
+                    reject(new Error('Could not get public keys'));
+                });
+                tx.onError((error) => {
+                    console.error('newDriveKeyPEM error', error);
+                    reject(new Error('Could not get public keys'));
+                });
+                tx.send();
+            });
+
+            /* eslint-disable-next-line */
+            const newDriveKeyB64 = Array.from(newDriveKeyPEM.toString().matchAll(/KEY-----\s+(\S*?)\s+-----/g))[0][1];
+            console.info('New drive key (base64):', newDriveKeyB64);
+            const newDriveKeyContent = Utils.fromBase64(newDriveKeyB64);
+            fs.writeFileSync(driveKeyPath, newDriveKeyContent);
+        }
+        const driveKeyContent = fs.readFileSync(driveKeyPath);
+        KeyHolder.driveKey = await subtle.importKey(
+            'spki',
+            driveKeyContent,
+            {
+                namedCurve: 'P-256',
+                name: 'ECDSA'
+            },
+            true,
+            ['verify']
+        );
+
+        console.log('Keys loaded successfully');
+    }
+}
+
+export default KeyHolder;
