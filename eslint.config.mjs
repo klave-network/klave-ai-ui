ad6cc3a83030cf8056b84f8049cdefa0b873fc39
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-10-01 13:38:36 +0200

    chore: Add script to run uploader

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,7 +22,7 @@ export default antfu(
             'no-console': ['warn'],
             'antfu/no-top-level-await': ['off'],
             'node/prefer-global/process': ['off'],
-            'node/no-process-env': ['error'],
+            'node/no-process-env': ['off'],
             'perfectionist/sort-imports': [
                 'error',
                 {
