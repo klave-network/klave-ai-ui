1cac2338c23b94dc3327939f9eb35cf8d572dbd7
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-10-14 11:31:08 +0200

    chore: Remove Spinner props

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,9 +1,9 @@
-export function Spinner({ height = 24, width = 24 }: { height?: number; width?: number }) {
+export function Spinner() {
     return (
         <svg
-            width={width}
-            height={height}
-            viewBox={`0 0 ${width} ${height}`}
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
             role="img"
             aria-label="Loading spinner"
